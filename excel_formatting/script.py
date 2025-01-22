f6bfddbad2b498064b3dfa0b0e8ccef4f315b436
ottojonas <ottobjonas@outlook.com>
2025-01-22 13:20:11 +0000

    formatter

--- excel_formatting/script.py
+++ excel_formatting/script.py
@@ -1,11 +1,13 @@
-import pandas as pd 
+import pandas as pd
 
-# load excel file 
-file_path = ''
+# load excel file
+file_path = "excel_formatting/storelist.xlsx"
 df = pd.read_excel(file_path)
 
-df[['numbers', 'product', 'city']] = df.iloc[:, 0].str.extract(r'(\d+)\s+(Blackthorn Sea Salt Flakes)\s+(.+)')
+df[["numbers", "product", "city"]] = df.iloc[:, 0].str.extract(
+    r"(\d+)\s+(Blackthorn Sea Salt Flakes)\s+(.+)"
+)
 
 # save to new excel file
-output_file_path = ''
+output_file_path = "excel_formatting/modified_storelist.xlsx"
 df.to_excel(output_file_path, index=False)
