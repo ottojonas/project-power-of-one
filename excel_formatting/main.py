4d5200f6ae7debfe89258e35272fb247ddf779e4
ottojonas <ottobjonas@outlook.com>
2025-01-22 13:29:33 +0000

    spelling never was my thing

--- excel_formatting/main.py
+++ excel_formatting/main.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # load excel file
-file_path = ""
+file_path = "excel_formatting/excel-sheets/modified_storelist.xlsx"
 df = pd.read_excel(file_path)
 
 df[["numbers", "product", "city"]] = df.iloc[:, 0].str.extract(
-    r"(\d+)\s+(Blackthorn Sea Salt Flakes)\s+(.+)"
+    r"(\d+)\s+(Blackthorn Sea Salt Flks)\s+(.+)"
 )
 
 # save to new excel file
-output_file_path = "excel_formatting/modified_storelist.xlsx"
+output_file_path = "excel_formatting/excel-sheets/modified_storelist2.xlsx"
 df.to_excel(output_file_path, index=False)
