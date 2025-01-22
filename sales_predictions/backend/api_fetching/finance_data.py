428778663d438f8d048b04502a06edb0a863f74c
ottojonas <ottobjonas@outlook.com>
2025-01-22 13:09:54 +0000

    finance init

---
+++ sales_predictions/backend/api_fetching/finance_data.py
@@ -0,0 +1,6 @@
+import yfinance as yf 
+import requests
+from icecream import ic 
+
+class YahooFinanceData(): 
+
--- sales_predictions/backend/api_fetching/weather_data.py
+++ sales_predictions/backend/api_fetching/weather_data.py
@@ -3,15 +3,20 @@ import os
 import requests
 from icecream import ic
 
-load_dotenv()
+class WeatherBitData:
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv("WEATHERBIT_API_KEY")
 
-weatherbit_api_key = os.getenv("WEATHERBIT_API_KEY")
-
-class WeatherBitData: 
-    def fetch_weatherbit_data(self, city, country, api_key):
-        url = f"https://api.weatherbit.io/v2.0/current?city={city},{country}&key={api_key}"
+    def fetch_weatherbit_data(self, city, country):
+        url = f"https://api.weatherbit.io/v2.0/current?city={city},{country}&key={self.api_key}"
         response = requests.get(url)
         return response.json()
 
-    weatherbit_data = fetch_weatherbit_data("City", "Country", weatherbit_api_key)
-    ic(f"weatherbit api data: {weatherbit_data}")
+    def display_weatherbit_data(self, city, country):
+        weatherbit_data = self.fetch_weatherbit_data(city, country)
+        ic(f"weatherbit api data: {weatherbit_data}")
+
+if __name__ == "__main__":
+    weather_data = WeatherBitData()
+    weather_data.display_weatherbit_data("City", "Country")
